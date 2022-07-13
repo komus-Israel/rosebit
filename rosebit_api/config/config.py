019937ff9766171407539d5782475ca2df970708
komus-Israel <komolehinisrael@gmail.com>
2022-07-13 16:06:18 +0100

    installed flask migrate

---
+++ rosebit_api/config/config.py
@@ -0,0 +1,48 @@
+import os
+
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config:
+
+    #JWT_SECRET_KEY =  os.environ.get("SECRET_KEY")
+    #JWT_ACCESS_TOKEN_EXPIRES = True
+    
+
+    @staticmethod
+    def init_app(app):
+        pass
+
+
+
+class DevelopmentConfig(Config):
+
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_DEV_URI")
+
+class ProductionConfig(Config):
+
+    DEBUG = True
+
+class TestConfig(Config):
+
+    
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI =  os.environ.get("SQLALCHEMY_DATABASE_TEST_URI")
+    
+    
+
+class StagingConfig(Config):
+
+    DEBUG = True
+    
+    
+
+config = {
+    'development':DevelopmentConfig,
+    'production':ProductionConfig,
+    'default':DevelopmentConfig,
+    'test':TestConfig,
+    'staging': StagingConfig
+}
